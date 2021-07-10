29e67edbf38defa29941795650f9107f26f63d0c
Nikita Isaev <keller108@yandex.ru>
2021-07-10 22:28:33 +0500

    route user is done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const { PORT = 3000 } = process.env;
 
-const { PORT } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
-    console.log('App is listening on port ${PORT}')
+    console.log(`App is listening on port ${PORT}`)
 })
